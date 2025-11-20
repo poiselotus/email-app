67d6b0fba6ef6f02170bb909841cc0f2923aa953
Wealthometer <wealthhajoh87@gmail.com>
2025-11-20 16:22:45 +0100

    ...

---
+++ src/AppRoutes.jsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import Mailbox1 from "./MailBox1";
+import Mailboxes from "./Mailboxes";
+import EmailDashboard from "./components/EmailDashboard";
+// import Mailbox2 from "./Mailbox2";
+// import Users from "./Users";
+// … your other pages
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/mailbox1" element={<Mailbox1 />} />
+      <Route path="/mailboxes" element={<Mailboxes />} />
+      <Route path="/email" element={<EmailDashboard />} />
+      {/* <Route path="/mailbox2" element={<Mailbox2 />} /> 
+
+      <Route path="/users" element={<Users />} />
+      <Route path="/companies" element={<Company />} />
+      <Route path="/blocked" element={<Blocked />} /> */}
+    </Routes>
+  );
+}
+
+ 
